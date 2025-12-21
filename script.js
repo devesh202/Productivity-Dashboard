1a1fed3e2fbfec1d00a0958422f114ef0965c7ae
devesh202 <deveshzope2002@gmail.com>
2025-12-22 00:03:12 +0530

    todo page is under progressb only mark as completed remaining.

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
-var allElems = document.querySelectorAll(".elem");
-var allFullElems = document.querySelectorAll('.fullElem');
-var allFullElemsBackBtn = document.querySelectorAll('.fullElem .back');
-// on clicking the elems each page should appear
-allElems.forEach((elem) => {
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var allFullElems = document.querySelectorAll(".fullElem");
+  var allFullElemsBackBtn = document.querySelectorAll(".fullElem .back");
+  // on clicking the elems each page should appear
+  allElems.forEach((elem) => {
     elem.addEventListener("click", () => {
-        var id =elem.id
-        allFullElems[id].style.display = 'block'
-    })
-})
+      var id = elem.id;
+      allFullElems[id].style.display = "block";
+    });
+  });
 
-allFullElemsBackBtn.forEach((btn) => {
+  allFullElemsBackBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
-        allFullElems[btn.id].style.display = 'none'
+      allFullElems[btn.id].style.display = "none";
+    });
+  });
+}
+
+
+openFeatures();
+
+var currTask=[]
+
+if(localStorage.getItem('currTask')){
+    currTask=JSON.parse(localStorage.getItem('currTask'))
+}
+else{
+    console.log('task list is empty');
+}
+
+
+
+function renderTask(){
+    var allTask = document.querySelector('.allTask');
+var sum = '';
+
+currTask.forEach((task) => {
+    sum += `<div class="task">
+    <h5>${task.task} <span class=${task.imp}>imp</span></h5>
+    <button>Mark as Completed</button>
+</div>`
+})
+allTask.innerHTML = sum
+
+}
+
+renderTask()
+
+
+let taskInput = document.querySelector('.addTask form input')
+let form = document.querySelector('.addTask form')
+let taskDetailsInput = document.querySelector('.addTask form textarea')
+let taskCheckbox=document.querySelector('.addTask form input[type="checkbox"]')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // console.log('hello') //we will not able to see this in console as it refreshes the page. 
+    // to get values of input from form use .value
+    let task = taskInput.value
+    let taskDetails = taskDetailsInput.value 
+    let checked =  taskCheckbox.checked
+    currTask.push({
+        task:task,
+        details:taskDetails,
+        imp:checked
     })
+    localStorage.setItem('currTask',JSON.stringify(currTask))
+    renderTask()
+    form.reset()
 })
